2489aead917e74bc01d62d73aa4420ee6ef0cbe6
Andy Edwards <jedwards@fastmail.com>
2017-05-06 14:08:31 -0500

    feat(UnitType): add unitsById map back

--- src/main/java/org/andork/unit/UnitType.java
+++ src/main/java/org/andork/unit/UnitType.java
@@ -2,15 +2,20 @@ package org.andork.unit;
 
 import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 public abstract class UnitType<T extends UnitType<T>> {
 	private final Set<Unit<T>> units = new LinkedHashSet<>();
+	private final Map<String, Unit<T>> unitsById = new HashMap<>();
 	private final Set<Unit<T>> unmodifiableUnits = Collections.unmodifiableSet(units);
+	private final Map<String, Unit<T>> unmodifiableUnitsById = Collections.unmodifiableMap(unitsById);
 
 	protected final void addUnit(Unit<T> unit) {
 		units.add(unit);
+		unitsById.put(unit.id, unit);
 	}
 
 	public double convert(double d, Unit<T> from, Unit<T> to) {
@@ -23,5 +28,9 @@ public abstract class UnitType<T extends UnitType<T>> {
 
 	public final Set<Unit<T>> units() {
 		return unmodifiableUnits;
+	}
+	
+	public final Map<String, Unit<T>> unitsById() {
+		return unmodifiableUnitsById;
 	}
 }
